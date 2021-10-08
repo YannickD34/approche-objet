c725f5b5c9436e359c2ebd0854e69c4f32cca4fb
Yannick Dubreuil <yannick.dub.7@gmail.com>
2021-10-08 10:38:20 +0200

    TP4 terminé

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		
+		String chaine = "12";
+		int result = Integer.parseInt(chaine);
+		System.out.println(result);
+		
+		int a = 5;
+		int b = 3;
+		int maximum = Integer.max(a, b);
+		System.out.println(maximum);
+
+	}
+
+}
